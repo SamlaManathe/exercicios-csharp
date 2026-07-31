f44284761f9d09c6c7028781d7094d478ee76d6f
SamlaManathe <samlamanathe@gmail.com>
2026-07-30 21:52:08 -0300

    Insere a resolução do exercicio 1: soma de dois números inteiros

--- Exercicios/Ex01_SomaNumeros.cs
+++ Exercicios/Ex01_SomaNumeros.cs
@@ -4,7 +4,24 @@ class Ex01_SomaNumeros
 {
     public static void Executar ()
     {
-        
-        
+        int soma = 0;
+
+        for(int i = 0; i < 2; i++)
+        {
+            Console.Write("\nInsira um número inteiro: ");
+            string? entrada = Console.ReadLine();
+
+            if(int.TryParse(entrada, out int numero))
+            {
+                soma += numero;
+            }
+            else
+            {
+                Console.WriteLine("\nEntrada inválida!");
+                return;
+            }
+        } 
+
+        Console.WriteLine($"\nSoma: {soma}");
     }
 }
